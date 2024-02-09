80a1551eedc2edbe33d29f80aecb4c5b1a2fb624
szmatolot <sopelameliaa@gmail.com>
2024-02-09 20:04:34 +0100

    Licznik Obwarzanów
    liczom sie na ekranie :DD

--- Gra_Gołąb/Assets/Scenes/ItemCollector.cs
+++ Gra_Gołąb/Assets/Scenes/ItemCollector.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
+    public TMP_Text obwarzankiText;
     private int obwarzanki = 0;
-    //[SerializeField] private Text obwarzankiText;
+
+
+    void Start()
+    {
+        obwarzankiText.text = "obwarzanki: " + obwarzanki.ToString();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
         {
@@ -15,9 +22,7 @@ public class ItemCollector : MonoBehaviour
                 Destroy(collision.gameObject);
                 obwarzanki++;
                 Debug.Log("obwarzanki:" + obwarzanki);
-                //obwarzankiText.text = "obwarzanki:" + obwarzanki;
+                obwarzankiText.text = "obwarzanki: " + obwarzanki.ToString();
             }
         }
 }
-
-// to co zakomentowane nie dziala nw czemu!
