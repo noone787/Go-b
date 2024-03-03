8548c572f77ec1f1e587bbf85f9eb9174f8562cf
noone787 <kubacieslik5407@gmail.com>
2024-03-03 11:47:44 +0100

    d

--- Gra_Gołąb/Assets/Scenes/Ziemia_Przenikalna.cs
+++ Gra_Gołąb/Assets/Scenes/Ziemia_Przenikalna.cs
@@ -4,31 +4,34 @@ using UnityEngine;
 
 public class Ziemia_Przenikalna : MonoBehaviour
 {
-    public Transform transform;
-    Transform transform1;
-    public float odległosc;
-    public float poczatek;
     
+    Transform transform1;
+    public Transform transform999;
+    double x1;
+    double y1;
     void Start()
     {   
-        
-        
+        transform1 = GetComponent<Transform>();
+        // Jeśli zmieniasz grubość przenikalnej platformy to tutaj x i y zmieniaj jeśli chcesz płynne przejście z góry na dół i na odwrót
+        x1 = 1.3;
+        y1 = 0.7;
     }
     
    
     void Update()
     {
         bool x = Input.GetKey(KeyCode.S);
-        if (transform.position.y > poczatek+ odległosc && x != true ) {
+        if (transform999.position.y > transform1.position.y + x1 && x != true ) {
             gameObject.layer = 0;
         }
         if (Input.GetKey(KeyCode.S)) {
             gameObject.layer = 1;
         }
         
-        else if (transform.position.y < poczatek + odległosc - 0.5) {
+        else if (transform999.position.y < transform1.position.y - y1) {
             gameObject.layer = 1;
         }
+        
 
         
     }
