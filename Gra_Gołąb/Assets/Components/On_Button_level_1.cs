4347dd3838023fb0e68793a98eb23546c54489e4
szmatolot <sopelameliaa@gmail.com>
2024-04-19 20:57:48 +0200

    level menu
    dodanie level menu

--- Gra_Gołąb/Assets/Components/On_Button_level_1.cs
+++ Gra_Gołąb/Assets/Components/On_Button_level_1.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class On_Play_Button_Click : MonoBehaviour
+public class On_Button_level_1 : MonoBehaviour
 {
-    [SerializeField] private string gra = "Level_Menu";
+    [SerializeField] private string gra = "main";
 
-    public void NewGameButton() 
-    {
+    public void NewGameButton() {
         SceneManager.LoadScene(gra);
     }
     
