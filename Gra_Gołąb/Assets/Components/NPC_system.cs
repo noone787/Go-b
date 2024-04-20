a3beaefa216f37e5625a82edd80c6fe2680ad2c0
szmatolot <sopelameliaa@gmail.com>
2024-04-20 17:39:38 +0200

    Dialog
    mozna gadac z golebiem

---
+++ Gra_Gołąb/Assets/Components/NPC_system.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using TMPro;
+using UnityEditor;
+
+public class NPC_system : MonoBehaviour
+{
+    [SerializeField] GameObject dialogBox;
+    [SerializeField] TMP_Text dialogText;
+    bool player_detection = false;
+    void Start()
+    {
+        StartCoroutine(dialog());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    IEnumerator dialog()
+    {
+        if (player_detection && Input.GetKeyDown(KeyCode.F) && !PlayerMovement.PlayerDialogue)
+        {
+            Debug.Log("dialog");
+            PlayerMovement.PlayerDialogue = true;
+            dialogBox.SetActive(true);
+            dialogText.text = "kupka w dupce chlupce";
+            yield return new WaitForSeconds(2);
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                PlayerMovement.PlayerDialogue = false;
+                dialogBox.SetActive(false);
+            }
+        }
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            player_detection = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        player_detection = false;
+    }
+
+}
--- Gra_Gołąb/Assets/Components/PlayerMovement.cs
+++ Gra_Gołąb/Assets/Components/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float MovementSpeed = 5;
+    public static bool PlayerDialogue = false;
     private void Start()
     {
         
@@ -12,7 +13,10 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        if (!PlayerDialogue)
+        {
         var movement = Input.GetAxisRaw("Horizontal"); 
         transform.position += new Vector3(movement,0,0) * Time.deltaTime * MovementSpeed;
+        }
     }
 }
