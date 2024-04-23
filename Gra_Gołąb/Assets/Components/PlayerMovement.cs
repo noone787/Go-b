47b476501472cab5328bbaab08707c94019049cb
szmatolot <sopelameliaa@gmail.com>
2024-04-23 21:40:02 +0200

    flip when change direction

--- Gra_Gołąb/Assets/Components/PlayerMovement.cs
+++ Gra_Gołąb/Assets/Components/PlayerMovement.cs
@@ -6,17 +6,27 @@ public class PlayerMovement : MonoBehaviour
 {
     public float MovementSpeed = 5;
     public static bool PlayerDialogue = false;
+    float movement;
+    private SpriteRenderer spriteRenderer;
     private void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
         if (!PlayerDialogue)
         {
-        var movement = Input.GetAxisRaw("Horizontal"); 
+        movement = Input.GetAxisRaw("Horizontal"); 
         transform.position += new Vector3(movement,0,0) * Time.deltaTime * MovementSpeed;
+        }
+        if (movement > 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        if (movement < 0)
+        {
+            spriteRenderer.flipX = false;
         }
     }
 }
