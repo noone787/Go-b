83d6ec600a37196b804392f5571195103e12d59f
szmatolot <sopelameliaa@gmail.com>
2024-04-23 17:47:03 +0200

    move death screen to ui

--- Gra_Gołąb/Assets/Components/Health.cs
+++ Gra_Gołąb/Assets/Components/Health.cs
@@ -16,8 +16,7 @@ public class Health : MonoBehaviour
     float zycie_poczatkowe;
     public Transform gracz;
     public TextMeshProUGUI textMeshPro;
-    public UnityEngine.UI.Image you_died;
-    public UnityEngine.UI.Image czarne_tło;
+    public GameObject death_image;
     public UnityEngine.UI.Image image;
     public UnityEngine.UI.Image Play_Button_Image;
     public UnityEngine.UI.Image Options_Button_Image;
@@ -35,14 +34,12 @@ public class Health : MonoBehaviour
     void Update(){
         image.fillAmount = health/100;
         if (health == 0) {
-            czarne_tło.enabled = true;
-            you_died.enabled = true;
+            death_image.SetActive(true);
             Time.timeScale = 0f;
             textMeshPro.enabled = false;
             if (Input.GetKeyDown(KeyCode.Space)) {
                 health = zycie_poczatkowe;
-                czarne_tło.enabled = false;
-                you_died.enabled = false;
+                death_image.SetActive(false);
                 textMeshPro.enabled = true;
                 gracz.position = x;
             }
