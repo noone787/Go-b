853b0cf16c12109285d6d92c67dc3fc1d0dedde5
szmatolot <sopelameliaa@gmail.com>
2024-04-27 14:11:43 +0200

    mgla

--- Gra_Gołąb/Assets/Components/CHangeLayerTo.cs
+++ Gra_Gołąb/Assets/Components/CHangeLayerTo.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class CHangeLayerTo : MonoBehaviour
 {
     public GameObject[] platforms;
+    public GameObject Shadow;
     void OnTriggerEnter2D()
     {
         for (int i = 0; i < platforms.Length; i++)
-        platforms[i].SetActive(true);
+        {
+            platforms[i].SetActive(true);
+        }
+        Shadow.SetActive(false);
     }
 }
