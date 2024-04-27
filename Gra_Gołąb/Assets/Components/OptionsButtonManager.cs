8d33e1c1c2b4bf2511e1c9064f7c9b68a90263e7
szmatolot <sopelameliaa@gmail.com>
2024-04-27 13:54:00 +0200

    changing resolution works(after build)

--- Gra_Gołąb/Assets/Components/OptionsButtonManager.cs
+++ Gra_Gołąb/Assets/Components/OptionsButtonManager.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using TMPro;
 
 public class OptionsButtonManager : MonoBehaviour
 {
     public AudioMixer audioMixer;
     [SerializeField] private string mainmenu = "MainMenu";
 
-    public Dropdown resolutionDropdown;
+    public TMP_Dropdown resolutionDropdown;
     Resolution[] resolutions;
 
     void Start()
@@ -18,12 +19,26 @@ public class OptionsButtonManager : MonoBehaviour
         resolutions = Screen.resolutions; 
         resolutionDropdown.ClearOptions();
         List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
         for (int i = 0; i <resolutions.Length; i++)
         {
             string option = resolutions[i].width + "x" + resolutions[i].height;
             options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
     public void BackToMain() 
