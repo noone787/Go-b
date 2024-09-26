600256e436e08df1a67c3ba84c2ea2e448da1408
noone787 <kubacieslik5407@gmail.com>
2024-09-26 15:52:17 +0200

    eq

--- Gra_Gołąb/Assets/Components/Buttom_Inventory.cs
+++ Gra_Gołąb/Assets/Components/Buttom_Inventory.cs
@@ -8,23 +8,23 @@ public class Buttom_Inventory : MonoBehaviour
     public Button button;
     private RectTransform rectTransform;
     public int slot;
-    public string przedmiot1;
+    public string[] przedmiot1;
     
     public Inventory_Managment inventory_Managment;
     void Start() {
         button.GetComponent<Button>();
-        slot = GetComponentInParent<Slot_Manager>().slot;
-        przedmiot1 = GetComponentInParent<Slot_Manager>().miejsce;
+        
+        przedmiot1 = GetComponentInParent<Inventory_Managment>().Inventory;
         
         
     }
     void Update() {
-        przedmiot1 = GetComponentInParent<Slot_Manager>().miejsce;
+        przedmiot1 = GetComponentInParent<Inventory_Managment>().Inventory;
     }
     public void ButtonClick() {
-        if (przedmiot1 != "null") {
-            inventory_Managment.dodanie_uzbrojenie = przedmiot1;
-            inventory_Managment.Inventory[GetComponentInParent<Slot_Manager>().slot] = "null";
+        if (przedmiot1[slot] != "null") {
+            inventory_Managment.dodanie_uzbrojenie = przedmiot1[slot];
+            inventory_Managment.Inventory[slot] = "null";
         }
         else {
 
--- Gra_Gołąb/Assets/Components/Button_Uzbrojenie.cs
+++ Gra_Gołąb/Assets/Components/Button_Uzbrojenie.cs
@@ -7,6 +7,7 @@ public class Button_Uzbrojenie : MonoBehaviour
 {   
     public Inventory_Managment inventory_Managment;
     public Button button;
+    public int slot;
     
 
     void Start() {
@@ -14,8 +15,8 @@ public class Button_Uzbrojenie : MonoBehaviour
     }
     public void ButtonClick_Uzbrojenie() {
         
-        inventory_Managment.dodanie = GetComponentInParent<Uzbrojenie_Manager>().przedmiot2; 
-        inventory_Managment.Inventory[GetComponentInParent<Uzbrojenie_Manager>().slot] = "null";
+        inventory_Managment.dodanie = GetComponentInParent<Inventory_Managment>().Inventory[slot]; 
+        inventory_Managment.Inventory[slot] = "null";
         
         
         }
